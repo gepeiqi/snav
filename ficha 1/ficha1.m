03215110bf19e4e4b1dde8e0c2c51a37b16d141c
JoaoAndrade <joao.migandrade@gmail.com>
2014-02-26 17:26:35 +0000

    added highest altitude & cumulative elevation

--- ficha 1/ficha1.m
+++ ficha 1/ficha1.m
@@ -21,14 +21,13 @@ data=[];
 for j=1:counter
     message = fgetl(file);
     if strcmp(message(2:6),'GPGGA')==1
-        data=[data;zeros(1,3)];
+        data=[data;zeros(1,8)];
         message=message(8:end);
         [input,message]=strtok(message,',');
         input=str2num(input);
         data(end,1)=input;
         message=message(2:end);
         [input,message]=strtok(message,',');
-        
         degrees=str2num(input(1:2));
         minutes=str2num(input(3:end));
         
@@ -48,7 +47,26 @@ for j=1:counter
         if message(2)=='E', data(end,3)=input;
         else data(end,3)=-input;
         end
-   
+        
+        message=message(4:end);
+        [input,message]=strtok(message,',');
+        if strcmp(input,'0')==1
+            [data,~]=removerows(data,size(data,1));
+            continue;
+        end
+        data(end,4)=str2num(input);
+        message=message(2:end);
+        [input,message]=strtok(message,',');
+        data(end,5)=str2num(input);
+        message=message(2:end);
+        [input,message]=strtok(message,',');
+        data(end,6)=str2num(input);
+        message=message(2:end);
+        [input,message]=strtok(message,',');
+        data(end,7)=str2num(input);
+        message=message(4:end);
+        [input,message]=strtok(message,',');
+        data(end,8)=str2num(input);
     else continue
     end
 end
@@ -57,5 +75,14 @@ minlat=min(data(:,2));
 maxlat=max(data(:,2));
 minlon=min(data(:,3));
 maxlon=max(data(:,3));
+maxalt=max(data(:,7));
 
-plot(data(:,3),data(:,2));
+ce=0;
+
+for i=2:counter
+    dh=data(i,7)-data(i-1,7)
+    if dh>0
+        ce=ce+dh;
+    end
+end
+
