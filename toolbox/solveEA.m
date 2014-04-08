b79ca24775a42bfdda8fdd4e7b76cab279061971
JoaoAndrade <joao.migandrade@gmail.com>
2014-04-08 11:42:39 +0100

    function that solves the eccentric anomaly equation

---
+++ toolbox/solveEA.m
@@ -0,0 +1,22 @@
+function [E] = solveEA(M,e,t)
+%SOLVEEA Retrieves Eccentric anomaly from mean anomaly
+%   Detailed explanation goes here
+
+if nargin==2
+    t=10^-12;
+end
+
+E=M;
+
+while (1)
+    Ep=E;
+    E=M+e*sin(E);
+    if abs(E-Ep)<t
+        break
+    else
+        continue
+    end
+end
+
+end
+
