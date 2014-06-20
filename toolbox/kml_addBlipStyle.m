98b0fb79d1e32782f69baf6d1d00b147d609269a
joandrade <joao.migandrade@gmail.com>
2014-06-20 12:45:00 +0100

    kml_addBlipStyle was missing. here it is.

---
+++ toolbox/kml_addBlipStyle.m
@@ -0,0 +1,12 @@
+function kml_addBlipStyle(file)
+%KML_ADDBLIPSTYLE Adds the blip icon to the kml file
+
+fprintf(file,'\n\t<Style id="blipStyle">');
+fprintf(file,'\n\t\t<IconStyle>');
+fprintf(file,'\n\t\t\t<Icon><href>http://i.imgur.com/W1kp2Cl.png</href></Icon>');
+fprintf(file,'\n\t\t</IconStyle>');
+fprintf(file,'\n\t</Style>');
+
+
+end
+
