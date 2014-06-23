18d84ecf1577ab867beb0a979d0a738db75a86bb
joandrade <joao.migandrade@gmail.com>
2014-06-23 15:20:12 +0100

    added kml_mask.m

---
+++ toolbox/kml_mask.m
@@ -0,0 +1,60 @@
+function kml_mask(lat,lon,h,mask)
+%KML_MASK Creates a KML file with all satellites visible above an elevation
+% angle
+
+[Xr,Yr,Zr]=llh2xyz(lat,lon,h);
+tst=getcurrenttime;
+
+latestparsed=fopen('latestparsed','r+');
+eph_file=sprintf('%s.eph',fscanf(latestparsed,'%s'));
+fclose(latestparsed);
+
+eph=load(eph_file);
+
+sats_XYZ=zeros(size(eph,1),4);
+
+for sat=1:size(eph,1)
+    sats_XYZ(sat,1)=eph(sat,1);
+    [sats_XYZ(sat,2),sats_XYZ(sat,3),sats_XYZ(sat,4)]=satpos(eph(sat,:),tst,eph(sat,4));
+end
+
+nsat=0;
+satList=zeros(1,4);
+
+for sat=1:size(eph,1)
+    [E,N,U]=ecef2enu(Xr,Yr,Zr,lat,lon,sats_XYZ(sat,2),sats_XYZ(sat,3),sats_XYZ(sat,4));
+    [az,elev]=azelev(E,N,U);
+    if elev<mask
+        continue
+    else
+        nsat=nsat+1;
+        satList(nsat,1)=sats_XYZ(sat,1);
+        satList(nsat,2)=sat;
+        satList(nsat,3)=az;
+        satList(nsat,4)=elev;
+    end
+end
+
+kml_sat_matrix=zeros(nsat,4);
+
+filename=sprintf('%2.4f_%3.4f_%d_%ddeg',lat,lon,uint32(tst),mask);
+file=kml_open(filename);
+kml_addGroundStyle(file);
+kml_addSatelliteStyles(file);
+kml_addPlacemark(file,'Ground Station','',[lat,lon,h],'ground');
+
+for n=1:nsat
+    kml_sat_matrix(n,1)=satList(n,1);
+    [kml_sat_matrix(n,2),kml_sat_matrix(n,3),kml_sat_matrix(n,4)]=...
+        xyz2llh(sats_XYZ(satList(n,2),2),sats_XYZ(satList(n,2),3),sats_XYZ(satList(n,2),4));
+    name=sprintf('SVN#%d',kml_sat_matrix(n,1));
+    description=sprintf('Latitude: %2.4f\nLongitude: %3.4f\nAzimuth: %3.4f\nElevation: %2.4f\n',...
+        kml_sat_matrix(n,2),kml_sat_matrix(n,3),satList(n,3),satList(n,4));
+    coordinates=[kml_sat_matrix(n,2),kml_sat_matrix(n,3),kml_sat_matrix(n,4)];
+    kml_addPlacemark(file,name,description,coordinates,'satellite');
+    kml_addPlacemark(file,'','',coordinates,'shadow')
+end
+
+kml_close(file);
+end
+
