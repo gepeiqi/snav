fbf12dcd4d92e416f4d0d42f015cb087ed705a47
joandrade <joao.migandrade@gmail.com>
2014-06-23 21:13:33 +0100

    icon scale fixed

--- toolbox/kml_addBlipStyle.m
+++ toolbox/kml_addBlipStyle.m
@@ -4,6 +4,7 @@ function kml_addBlipStyle(file)
 fprintf(file,'\n\t<Style id="blipStyle">');
 fprintf(file,'\n\t\t<IconStyle>');
 fprintf(file,'\n\t\t\t<Icon><href>http://i.imgur.com/W1kp2Cl.png</href></Icon>');
+fprintf(file,'\n\t\t\t<scale>12</scale>');
 fprintf(file,'\n\t\t</IconStyle>');
 fprintf(file,'\n\t</Style>');
 
