72e79f16031030f56bfa18bd151d6d1a9a05c956
joandrade <joao.migandrade@gmail.com>
2014-06-23 16:09:38 +0100

    Removed ignored files

--- toolbox/kml_placeSatellite.m
+++ toolbox/kml_placeSatellite.m
@@ -7,8 +7,6 @@ coordinates=zeros(1,3);
 [coordinates(1,1),coordinates(1,2),coordinates(1,3)]=xyz2llh(X,Y,Z);
 description=sprintf('Position relative to ground:\n\t\tLatitude %fº\n\t\tLongitude: %fº\n\t\tAltitude: %.3f km',coordinates(1),coordinates(2),coordinates(3)/1000);
 
-coordinates(1,3)=2500000; % Just so it's on screen
-
 kml_addPlacemark(file,sprintf('SVN%d',data(1)),description,coordinates,'satellite')
 kml_addPlacemark(file,'','',coordinates,'shadow')
 end
