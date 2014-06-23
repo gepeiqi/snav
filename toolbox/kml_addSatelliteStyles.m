a2c9f4882df33a7903453ef18dc83dadf3c3a681
joandrade <joao.migandrade@gmail.com>
2014-06-23 15:20:44 +0100

    added <scale> property to the icon so it can be seen from high altitudes.

--- toolbox/kml_addSatelliteStyles.m
+++ toolbox/kml_addSatelliteStyles.m
@@ -4,6 +4,8 @@ function kml_addSatelliteStyles(file)
 fprintf(file,'\n\t<Style id="satelliteStyle">');
 fprintf(file,'\n\t\t<IconStyle>');
 fprintf(file,'\n\t\t\t<Icon><href>http://i.imgur.com/kABV40x.png</href></Icon>');
+fprintf(file,'\n\t\t\t<scale>16</scale>');
+
 fprintf(file,'\n\t\t</IconStyle>');
 fprintf(file,'\n\t</Style>');
 fprintf(file,'\n\t<Style id="shadowStyle">');
