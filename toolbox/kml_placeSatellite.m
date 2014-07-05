843916b400b13b6ae0935dd12709fa6113497750
joandrade <joao.migandrade@gmail.com>
2014-07-05 14:37:46 +0100

    added correction for Earth rotation

--- toolbox/kml_placeSatellite.m
+++ toolbox/kml_placeSatellite.m
@@ -7,6 +7,8 @@ if nargin ==3
     add_description='';
 end
 
+OmegaDot_e=7.2921151467*10^-5;
+
 coordinates=zeros(1,3);
 [coordinates(1,1),coordinates(1,2),coordinates(1,3)]=xyz2llh(X,Y,Z);
 description=sprintf('Position relative to ground:\n\t\tLatitude %fº\n\t\tLongitude: %fº\n\t\tAltitude: %.3f km\n%s',coordinates(1),coordinates(2),coordinates(3)/1000,add_description);
@@ -26,9 +28,22 @@ Tf=0:60:600;
 
 path=zeros(11,3);
 
-for i=1:11
+for i=1:(1+24*60)
     [Xp,Yp,Zp]=satpos(data,tst+Tf(i),data(4));
-    [path(i,1),path(i,2),path(i,3)]=xyz2llh(Xp,Yp,Zp);
+    [lat,lon,alt]=xyz2llh(Xp,Yp,Zp);
+    
+    % Longitude correction to account for Earth spin movement
+    lon=lon+OmegaDot_e*Tf(i)*180/pi;
+    
+    if lon>180
+        lon=lon-360;
+    elseif lon<-180
+        lon=lon+360;
+    end
+    
+    path(i,1)=lat;
+    path(i,2)=lon;
+    path(i,3)=alt;
 end
 
 kml_drawPath(file,path,'blue');
